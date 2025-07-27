a3fad24d227f960c7711b1207ba87983535593c6
Harsha260 <s.gnanasriharsha@gmail.com>
2025-07-27 17:36:28 +0530

    pre messages

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// const express = require('express');
-// const http = require('http');
-// const socketIo = require('socket.io');
-//
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIo(server);
-//
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-//
-// io.on('connection', (socket) => {
-//   console.log('A user connected');
-//
-//   socket.on('chat message', (msg) => {
-//     io.emit('chat message', msg); // Broadcast message to all connected clients
-//   });
-//
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected');
-//   });
-// });
-//
-// server.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
-
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -35,6 +6,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Store chat history
+const chatHistory = [];
+const MAX_HISTORY_LENGTH = 100; // Limit the number of messages to store
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -42,8 +17,17 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('chat message', (data) => { // Data will now be an object {username, message}
-    io.emit('chat message', data); // Broadcast the object to all connected clients
+  // Send the current chat history to the newly connected user
+  socket.emit('chat history', chatHistory);
+
+  socket.on('chat message', (data) => {
+    // Add the new message to history
+    chatHistory.push(data);
+    // Keep history length in check
+    if (chatHistory.length > MAX_HISTORY_LENGTH) {
+      chatHistory.shift(); // Remove the oldest message
+    }
+    io.emit('chat message', data); // Broadcast the new message to all connected clients
   });
 
   socket.on('disconnect', () => {
